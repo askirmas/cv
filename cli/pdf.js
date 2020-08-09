8fa3ee42d3f7265b2c6bb92221ecf2d6fdb6836d
askirmas <kirmas@phystech.edu>
2020-08-09 18:03:29 +0300

    Add content-* mixins for ordering things

--- cli/pdf.js
+++ cli/pdf.js
@@ -7,16 +7,15 @@ async function main() {
   const cwd = process.cwd()
   , browser = await puppeteer.launch()
   , page = await browser.newPage()
-
-  await page.goto(`file://${cwd}/Andrii_Kirmas.html`, {
-    waitUntil: "networkidle2"
-  });
-  await page.setViewport({ width: 1680, height: 1050 });
-  await page.pdf({
-    path: `${cwd}/Andrii_Kirmas.pdf`,
-    format: "A4",
-    printBackground: true
-  });
-
-  await browser.close();
+  
+    await page.goto(`file://${cwd}/Andrii_Kirmas.html`, {
+      waitUntil: "networkidle2"
+    })
+    await page.setViewport({ width: 1680, height: 1050 })
+    await page.pdf({
+      path: `${cwd}/Andrii_Kirmas.pdf`,
+      format: "A4",
+      printBackground: true
+    })
+    await browser.close()
 }
