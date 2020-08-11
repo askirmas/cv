22441a45e513e68f44f62ce998f5f50d912c18d4
Andrii Kirmas <kirmas@phystech.edu>
2020-08-11 19:44:20 +0300

    Change postinstall to be node script

---
+++ postinstall.js
@@ -0,0 +1,16 @@
+const os = require('os')
+, childProcess = require('child_process')
+
+let cmd = "nohup xdg-open index.html >/dev/null 2>&1 &"
+
+switch (os.platform()) {
+  case "win32":
+  case "cygwin":
+    cmd = 'start "" index.html'
+    break
+  case "darwin":
+    cmd = "open index.html"
+    break
+}
+
+childProcess.execSync(cmd)
