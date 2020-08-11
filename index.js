08e268e2486d85dd6c9996cae20545785a57587b
askirmas <kirmas@phystech.edu>
2020-08-11 23:14:14 +0300

    Reuse file to open from const string

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 
 const os = require('os')
 , childProcess = require('child_process')
+, file2open = "index.html"
 
-let cmd = "nohup xdg-open index.html >/dev/null 2>&1 &"
+let cmd = `nohup xdg-open ${file2open} >/dev/null 2>&1 &`
 
 switch (os.platform()) {
   case "win32":
   case "cygwin":
-    cmd = 'start "" index.html'
+    cmd = `start "" ${file2open}`
     break
   case "darwin":
-    cmd = "open index.html"
+    cmd = `open ${file2open}`
     break
 }
 
