41a3d94bdc7c2f7d010e56296a627d6bda7a2fa6
Andrii Kirmas <kirmas@phystech.edu>
2021-07-10 14:48:56 +0300

    99c6a2d1234dc01089e62fd066796d6326faf8a0

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4019adc6ece4b068ac88";
+/******/ 	var hotCurrentHash = "223fc21f0ba86fe44fcb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
